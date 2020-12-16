cecd68a3a2cb27492d09beb06c005f4f4b2986d0
mikie Reed <mikie@prenotions.com>
2020-12-16 15:14:09 -0800

    weather com parser

--- weatherterm/parsers/weather_com_parser.py
+++ weatherterm/parsers/weather_com_parser.py
@@ -1,4 +1,10 @@
+import re
+
 from weatherterm.core import ForecastType
+from weatherterm.core import Forecast
+from weatherterm.core import Request
+from weatherterm.core import Unit
+from weatherterm.core import UnitConverter
 
 
 class WeatherComParser:
@@ -10,6 +16,13 @@ class WeatherComParser:
             ForecastType.TENDAYS: self._five_and_ten_days_forecast,
             ForecastType.WEEKEND: self._weekend_forecast,
             }
+        self._base_url = 'http://weather.com/weather/{forecast}/1/{area}'
+        self._request = Request(self._base_url)
+
+        self._temp_regex = re.compile('([0-9]+)\D{,2}([0-9]+)')
+        self._only_digits_regex = re.compile('[0-9]+')
+
+        self._unit_converter = UnitConverter(Unit.FAHRENHEIT)
 
     def _today_forecast(self, args):
         raise NotImplementedError()
@@ -24,4 +37,18 @@ class WeatherComParser:
         self._forecast_type = args.forecast_option
         forecast_function = self._forecast[args.forecast_option]
         return forecast_function(args)
+
+    def _get_data(self, container, search_items):
+        scraped_data = {}
+
+        for key, value in search_items.items():
+            result = container.find(value, class_=key)
+
+            data = None if result is None else result.get_text()
+            
+            if data is not None:
+                scraped_data[key] = data
+
+        return scraped_data
+
         
